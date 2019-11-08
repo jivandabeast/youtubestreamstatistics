dd4737c9d53704a6cb3b34c631c2ebdc1b55668c
jivandabeast <jivanramjisingh@gmail.com>
2019-11-08 11:08:27 -0500

    Fixing some consistency issues

--- ViewCountWithTime.sh
+++ ViewCountWithTime.sh
@@ -3,9 +3,9 @@
 #Set variable for file where the script will store the data, update accordingly
 file=[DataFile]
 
-#Grabs the date, appends to the end of the file, uncomment other line for epoch time 
-date +"%m/%d/%Y %T">> $file
-#date +%s
+#Grabs the date, appends to the end of the file, uncomment other line for MM/DD/YYYY HH:MM:SS
+#date +"%m/%d/%Y %T">> $file
+date +%s >> $file
 
 #Grabs the current number of viewers on the livestream from the YouTube API, update with the stream ID
 curl -X GET "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=[STREAM ID HERE]&key=AIzaSyAzRmWRQKbQpnAIH-Ws0ruzgxafjECdBCg" > temp.json
